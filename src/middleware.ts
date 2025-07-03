64847203787182151f23ef6f0c1ee2c49e408bb6
Om Kakatkar <om@compose.co.in>
2025-07-03 19:08:23 +0530

    refactor: simplify next.config.js and update dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,4 @@ export const config = {
     // Run on everything but Next internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
-  runtime: "nodejs",
 };
