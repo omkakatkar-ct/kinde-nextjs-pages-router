3b6fce92abd40b521dedecb1db8def3d2f9367be
Om Kakatkar <om@compose.co.in>
2025-09-03 09:27:33 +0530

    refactor: enhance authentication feedback in my-account page

--- src/pages/my-account.tsx
+++ src/pages/my-account.tsx
@@ -1,12 +1,17 @@
+import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import Link from "next/link";
 
 export default function Dashboard() {
+  const { isAuthenticated } = useKindeAuth();
+
   return (
     <div className="container">
       <div className="card start-hero">
         <p className="text-body-2 start-hero-intro">Woohoo!</p>
         <p className="text-display-2">
-          Your authentication is all sorted.
+          {isAuthenticated
+            ? "Authenticating..."
+            : "Your authentication is all sorted."}
           <br />
           Build the important stuff.
         </p>
