29e35ba42624a1bf43c6c6b9ffac94b9ad2d8d25
Om Kakatkar <om@compose.co.in>
2025-09-03 10:27:42 +0530

    refactor: enhance authentication logging in my-account page

--- src/pages/my-account.tsx
+++ src/pages/my-account.tsx
@@ -2,20 +2,17 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import Link from "next/link";
 
 export default function Dashboard() {
-  const { isAuthenticated } = useKindeAuth();
+  const { isAuthenticated, accessTokenRaw } = useKindeAuth();
+
+  console.log("isAuthenticated", isAuthenticated);
+  console.log("accessTokenRaw", accessTokenRaw);
 
   return (
     <div className="container">
       <div className="card start-hero">
         <p className="text-body-2 start-hero-intro">Woohoo!</p>
-        <p className="text-display-2">
-          {isAuthenticated
-            ? "Authenticating..."
-            : "Your authentication is all sorted."}
-          <br />
-          Build the important stuff.
-        </p>
       </div>
+
       <section className="next-steps-section">
         <h2 className="text-heading-1">Next steps for you</h2>
         <Link href="/">Go to Home</Link>
